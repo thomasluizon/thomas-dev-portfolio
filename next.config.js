9cab80c0fdab1c1d95cb0e667554ec78a8598406
thomasluizon <thomaslrgregorio@gmail.com>
2023-09-03 02:17:01 -0300

    fixed internationalization middleware

--- next.config.js
+++ next.config.js
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	async redirects() {
-		return [
-			{
-				source: '/',
-				destination: '/en',
-				permanent: true,
-			},
-		]
-	},
+	trailingSlash: true,
+	// async redirects() {
+	// 	return [
+	// 		{
+	// 			source: '/',
+	// 			destination: '/en',
+	// 			permanent: true,
+	// 		},
+	// 	]
+	// },
 }
 
 module.exports = nextConfig
--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -20,7 +20,7 @@ export default async function LocaleLayout({
 	let messages
 
 	try {
-		messages = (await import(`@/app/messages/${locale}.json`)).default
+		messages = (await import(`@/messages/${locale}.json`)).default
 	} catch (error) {
 		notFound()
 	}
